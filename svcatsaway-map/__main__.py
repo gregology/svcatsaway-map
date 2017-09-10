5d211f6f0ae8b01eb08e2b7c678abfe8d20e5dc5
Greg Clarke <greg@gho.st>
2017-09-09 21:56:33 -0400

    pushing all of page content to page

--- svcatsaway-map/__main__.py
+++ svcatsaway-map/__main__.py
@@ -21,7 +21,81 @@ def collect_location():
   return response['locations'][0]
 
 def generate_body(location):
-  return '{"page": {"id": ' + options.page_id + ', "body_html": "<script>var latest_location = {lat: ' + str(location['lat']) + ', lng: ' + str(location['lon']) + ', timestamp: ' + "'" + str(location['timestamp']) + "'" + '};</script>"}}'
+  return json.dumps({
+    'page': {
+        'id': options.page_id,
+        'body_html': body_html(location)
+    }
+  })
+
+def body_html(location):
+    return """
+      <style>
+         #map {{
+          height: 400px;
+          width: 100%;
+         }}
+      </style>
+      
+      <h1 class="center">Our Current Location</h1>
+      <div class="feature_divider"></div>
+
+      <div id="map"></div>
+      <div id="timestamp"></div>
+
+      <script>
+        var latest_location = {{lat: {lat}, lng: {lon}, timestamp: '{timestamp}'}};
+        function initMap() {{
+          var map = new google.maps.Map(document.getElementById('map'), {{
+            zoom: 8,
+            center: latest_location
+          }});
+          var marker = new google.maps.Marker({{
+            position: latest_location,
+            map: map
+          }});
+        }}
+      </script>
+      <script async defer
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBrmtrrkEdR_z0z-hNIS4l5Zxx3cX2y7vI&callback=initMap">
+      </script>
+      <script>
+        function timeSince(date) {{
+
+          var seconds = Math.floor((new Date() - date) / 1000);
+
+          var interval = Math.floor(seconds / 31536000);
+
+          if (interval > 1) {{
+            return interval + " years";
+          }}
+          interval = Math.floor(seconds / 2592000);
+          if (interval > 1) {{
+            return interval + " months";
+          }}
+          interval = Math.floor(seconds / 86400);
+          if (interval > 1) {{
+            return interval + " days";
+          }}
+          interval = Math.floor(seconds / 3600);
+          if (interval > 1) {{
+            return interval + " hours";
+          }}
+          interval = Math.floor(seconds / 60);
+          if (interval > 1) {{
+            return interval + " minutes";
+          }}
+          return Math.floor(seconds) + " seconds";
+        }}
+      </script>
+      <script>
+        document.getElementById("timestamp").innerHTML = 'Last updated ' + timeSince(new Date(Date.parse(latest_location['timestamp']))) + ' ago. Note: this map will not update when we are stationary.';
+      </script>
+    """.format(
+      lat=location['lat'],
+      lon=location['lon'],
+      timestamp=location['timestamp'],
+    )
 
 def post_location(location):
   request_url = '/admin/pages/' + options.page_id + '.json'
